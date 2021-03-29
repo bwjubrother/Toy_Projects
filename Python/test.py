1b2c5821b9cfd47c41bd168073e7b3c820659ef9
bwjubrother <bwjubrother@gmail.com>
2021-03-29 16:46:07 +0900

    home

--- Python/test.py
+++ Python/test.py
@@ -1,19 +1,39 @@
-# import pyautogui as pag
+def solution(n, costs):
+    answer = 0
+    # sorted(costs, key = lambda x : x[2])
+    costs.sort(key = lambda x : x[2])
+    routes = {costs[0][0]}
+    for cost in costs:
+        print(cost)
+        if cost[0] in routes or cost[1] in routes:
+            if cost[0] in routes and cost[1] in routes:
+                continue
+            else:
+                routes.add(cost[0])
+                routes.add(cost[1])
+                answer += cost[2]
+        if len(routes) == n:
+            break
+    return answer
 
-# while True:
-#     x, y = pag.position()
-#     print('x: %s, y: %s' % (x, y))
 
-import time
+def solution2(n, costs):
+    # kruskal algorithm
+    ans = 0
+    costs.sort(key = lambda x: x[2]) 
+    routes = set([costs[0][0]]) 
+    while len(routes)!=n:
+        for cost in costs:
+            print(cost)
+            if cost[0] in routes and cost[1] in routes:
+                continue
+            if cost[0] in routes or cost[1] in routes:
+                routes.update([cost[0], cost[1]])
+                ans += cost[2]
+                break
+        print(routes)
+    return ans
 
-def countdown(t):
-    while t:
-        mins, secs = divmod(t, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeformat, end='\r')
-        time.sleep(1)
-        t -= 1
-    print('00:00', end='\r')
-    
-countdown(90)
-exit()
+
+print(solution(4, [[0, 1, 5], [1, 2, 3], [2, 3, 3], [3, 1, 2], [3, 0, 4]]))
+print(solution2(4, [[0, 1, 5], [1, 2, 3], [2, 3, 3], [3, 1, 2], [3, 0, 4]]))
